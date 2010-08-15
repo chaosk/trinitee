d6265237f0deea23ab6b7a26583eb336f0e3ef50
Krzysztof Socha <ksocha@ksocha.com>
2010-08-15 14:03:37 +0200

    added missing import

--- accounts/forms.py
+++ accounts/forms.py
@@ -1,4 +1,5 @@
 from django import forms
+from django.conf import settings
 from django.contrib.auth.models import User
 from django.core.exceptions import ImproperlyConfigured
 from django.core.files.images import get_image_dimensions
