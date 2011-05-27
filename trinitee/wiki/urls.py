c5ef33755b5f44e1c02fb720098a91c54116ba79
Krzysztof Socha <ksocha@ksocha.com>
2011-05-27 19:53:34 +0200

    changed wiki url patterns (scheme taken from github wikis)

--- trinitee/wiki/urls.py
+++ trinitee/wiki/urls.py
@@ -1,9 +1,13 @@
 from django.conf.urls.defaults import patterns, url
 
 urlpatterns = patterns('wiki.views',
-	url(r'^new/$', 'wiki_new', name='wiki_new'),
-	url(r'^history/(?P<slug>[\w\W]+)$', 'wiki_history', name='wiki_history'),
-	url(r'^edit/(?P<slug>[\w\W]+)$', 'wiki_edit', name='wiki_edit'),
-	url(r'^(?P<slug>[\w\W]+)$', 'wiki_detail', name='wiki_detail'),
+	url(r'^_new/?$', 'wiki_new', name='wiki_new'),
+	url(r'^(?P<slug>[^/]+)/_history/(?P<rev_from>\d+)\.\.\.(?P<rev_to>\d+)/$',
+	 	'wiki_compare', name='wiki_compare'),
+	url(r'^(?P<slug>[^/]+)/_history/(?P<rev>\d+)/$',
+		'wiki_history_detail', name='wiki_history_detail'),
+	url(r'^(?P<slug>[^/]+)/_history/$', 'wiki_history', name='wiki_history'),
+	url(r'^(?P<slug>[^/]+)/_edit/$', 'wiki_edit', name='wiki_edit'),
+	url(r'^(?P<slug>[^/]+)$', 'wiki_detail', name='wiki_detail'),
 	url(r'^$', 'wiki_index', name='wiki_index'),
 )
