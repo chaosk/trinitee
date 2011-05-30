34f7719ceeeedfbe66283785fb967de4c15a42c4
Krzysztof Socha <ksocha@ksocha.com>
2011-05-31 01:03:48 +0200

    added get_absolute_url to WikiPage model

--- trinitee/wiki/models.py
+++ trinitee/wiki/models.py
@@ -19,3 +19,7 @@ class WikiPage(models.Model):
 
 	def __unicode__(self):
 		return self.title
+
+	@models.permalink
+	def get_absolute_url(self):
+		return ('wiki.views.wiki_detail', (), {'slug': self.slug})
