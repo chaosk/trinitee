76a14695ea9ef4d8a2cd89252bc037da85867e3c
Krzysztof Socha <ksocha@ksocha.com>
2011-06-19 13:07:40 +0200

    added more tests to wiki app

--- trinitee/wiki/tests.py
+++ trinitee/wiki/tests.py
@@ -4,6 +4,7 @@ from django.utils import unittest
 from reversion.models import Version
 from wiki.models import WikiPage
 
+INVALID_SLUG = '_invalidslug'
 
 class WikiTestCase(unittest.TestCase):
 	def setUp(self):
@@ -26,7 +27,7 @@ class WikiTestCase(unittest.TestCase):
 
 	def testViewDetailFailure(self):
 		response = self.client.get(reverse('wiki_detail',
-			args=['_impossibleslug']))
+			args=[INVALID_SLUG]))
 		self.assertEqual(response.status_code, 404)
 
 	def testViewCreatePrepare(self):
@@ -58,6 +59,60 @@ class WikiTestCase(unittest.TestCase):
 		response = self.client.post(reverse('wiki_new'),
 			{'title': u"Page 1", 'content': "Test content"})
 		self.assertEqual(response.status_code, 200)
+
+	def testViewEditPrepareSuccess(self):
+		response = self.client.get(reverse('wiki_edit',
+			args=[self.page1.slug]))
+		self.assertEqual(response.status_code, 200)
+
+	def testViewEditPrepareFailure(self):
+		response = self.client.get(reverse('wiki_edit',
+			args=[INVALID_SLUG]))
+		self.assertEqual(response.status_code, 404)
+
+	def testViewEditSuccess(self):
+		new_content = "Page 1! Edited!"
+		response = self.client.post(reverse('wiki_edit',
+			args=[self.page1.slug]), {'content': new_content,
+				'comment': "Slightly updated"}, follow=True)
+		self.assertEqual(response.status_code, 200)
+		self.assertContains(response.redirect_chain[0][0],
+			self.page1.get_absolute_url())
+		self.assertEqual(response.context['page'].content, new_content)
+
+	def testViewEditFailure(self):
+		# Send no POST data
+		response = self.client.post(reverse('wiki_edit',
+			args=[self.page1.slug]))
+		self.assertEqual(response.status_code, 200)
+
+		# Try to edit a nonexistent Page
+		response = self.client.post(reverse('wiki_edit',
+			args=[INVALID_SLUG]))
+		self.assertEqual(response.status_code, 404)
+
+	def testViewDeletePrepareSuccess(self):
+		response = self.client.get(reverse('wiki_delete',
+			args=[self.page2.slug]))
+		self.assertEqual(response.status_code, 200)
+
+	def testViewDeletePrepareFailure(self):
+		response = self.client.get(reverse('wiki_delete',
+			args=[INVALID_SLUG]))
+		self.assertEqual(response.status_code, 404)
+
+	def testViewDeleteSuccess(self):
+		response = self.client.post(reverse('wiki_delete',
+			args=[self.page2.slug]), follow=True)
+		self.assertEqual(response.status_code, 200)
+		self.assertIn(reverse('wiki_index'), response.redirect_chain[0][0])
+		self.assertRaises(WikiPage.DoesNotExist, WikiPage.objects.get, pk=2)
+
+	def testViewDeleteFailure(self):
+		# Try to delete a nonexistent Page
+		response = self.client.post(reverse('wiki_delete',
+			args=[INVALID_SLUG]))
+		self.assertEqual(response.status_code, 404)
 
 	def testViewList(self):
 		response = self.client.get(reverse('wiki_list'))
